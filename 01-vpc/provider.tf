4ed19503a2a97b906eb6a159e259d28d71274526
rabbanimdr <rabbanimdr15@gmail.com>
2026-01-30 18:13:50 +0530

    new commit

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+      source = "hashicorp/aws"
+      version = "6.30.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  # Configuration options
 }
